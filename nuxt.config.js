509869d10b7b8fd23e9027ed63f9810e21b174da
Jared Cornwall <jaredkc@gmail.com>
2019-03-11 21:16:20 -0600

    Setup dotenv for Firebase config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,8 +1,19 @@
 const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
 const pkg = require('./package')
 
+require('dotenv').config()
+
 module.exports = {
   mode: 'spa',
+
+  env: {
+    API_KEY: process.env.API_KEY,
+    AUTH_DOMAIN: process.env.AUTH_DOMAIN,
+    DATABASE_URL: process.env.DATABASE_URL,
+    PROJECT_ID: process.env.PROJECT_ID,
+    STORAGE_BUCKET: process.env.STORAGE_BUCKET,
+    MESSAGING_SENDER_ID: process.env.MESSAGING_SENDER_ID
+  },
 
   /*
   ** Headers of the page
