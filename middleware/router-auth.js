4dc9be1363d3a4467291f016730b762e85d0c45f
Jared Cornwall <jaredkc@gmail.com>
2019-03-12 11:21:37 -0600

    Check for route directory instead, add sign in/up, account check

--- middleware/router-auth.js
+++ middleware/router-auth.js
@@ -1,19 +1,31 @@
 export default function({ store, redirect, route }) {
-  if (!isLogin(store) && isAdminRoute(route)) {
+  if (!isSignedIn(store) && (isAdminRoute(route) || isAccountRoute(route))) {
     return redirect('/signin')
+  }
+
+  if (isSignedIn(store) && isSignInRoute(route)) {
+    return redirect('/')
   }
 }
 
-const isLogin = store => {
+const isSignedIn = store => {
   return store && store.state && store.state.user.signedIn
 }
 
 const isAdminRoute = route => {
-  if (
-    route.matched.some(
-      record => record.path === '/admin' || record.path === '/account'
-    )
-  ) {
+  if (route.matched.some(record => record.path.includes('/admin'))) {
+    return true
+  }
+}
+
+const isAccountRoute = route => {
+  if (route.matched.some(record => record.path.includes('/account'))) {
+    return true
+  }
+}
+
+const isSignInRoute = route => {
+  if (route.matched.some(record => record.path.includes('/sign'))) {
     return true
   }
 }
