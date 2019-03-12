d1daf4d55db8a87d645c1623709af2b3b02776d5
Jared Cornwall <jaredkc@gmail.com>
2019-03-12 15:24:26 -0600

    Starting to add some informative content

--- README.md
+++ README.md
@@ -2,6 +2,21 @@
 
 > Starter Nuxt project for Firebase (Auth, Functions, Firestore, Hosting)
 
+## Firebase Setup
+
+Enable Email/Password and Google sign-in methods for Authentication, or make modifications as needed.
+
+Create `.env` with the following config options needed to initialize Firebase app.
+
+```
+API_KEY=
+AUTH_DOMAIN=
+DATABASE_URL=
+PROJECT_ID=
+STORAGE_BUCKET=
+MESSAGING_SENDER_ID=
+```
+
 ## Build Setup
 
 ``` bash
--- pages/index.vue
+++ pages/index.vue
@@ -11,56 +11,23 @@
     >
       <v-card>
         <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
+          Welcome
         </v-card-title>
         <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
           <p>
-            For more information on Vuetify, check out the <a
+            This is a starter template for <a
+              href="https://nuxtjs.org/"
+              target="_blank"
+            >Nuxt</a>, <a
               href="https://vuetifyjs.com"
               target="_blank"
-            >documentation</a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
+            >Vuetify</a>, and <a
+              href="https://firebase.google.com/"
               target="_blank"
-              title="chat"
-            >discord</a>.
+            >Firebase</a>.
           </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              title="contribute"
-            >issue board</a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-          >Nuxt Documentation</a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-          >Nuxt GitHub</a>
+          <p>To learn more visit this <a href="https://github.com/jaredkc/nuxt-firebase-starter">project on Github</a>.</p>
         </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            flat
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
       </v-card>
     </v-flex>
   </v-layout>
