eff0d0d9829ad36df597a36a534e432a49dafa88
Jared Cornwall <jkc@Jareds-MacBook-Pro.local>
2019-08-13 20:02:03 -0600

    Update Firebase config

--- fireinit.js
+++ fireinit.js
@@ -10,7 +10,8 @@ if (!firebase.apps.length) {
     databaseURL: process.env.DATABASE_URL,
     projectId: process.env.PROJECT_ID,
     storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID
   }
   firebase.initializeApp(config)
 }
